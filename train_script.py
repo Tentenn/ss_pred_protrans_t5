a9e4cd07584c62a19cd32260a9b3d203b3f0ca1c
Tentenn <kok.tang@gmx.de>
2022-06-20 23:21:50 +0000

    wandb integration, main_train_loop working

--- train_script.py
+++ train_script.py
@@ -1,8 +1,8 @@
+
 """
 This code trains the CNN for 3-State secondary structure prediction
 Using ProtTrans T5 per residue embeddings.
 """
-
 import torch
 from torch import nn
 import torch.optim as optim
@@ -19,10 +19,15 @@ import numpy as np
 import re
 import h5py
 from torch.nn.utils.rnn import pad_sequence
+from itertools import chain, repeat, islice
+from sklearn.metrics import accuracy_score
+import gc
+import wandb
 
 import utils
 from ConvNet import ConvNet
 
+gc.enable()
 
 class EmbedDataset(Dataset):
     """
@@ -68,15 +73,16 @@ def logits_to_preds(logits):
   @returns: a list of predictions eg. [2, 1, 1, 2, 2, 2, 0] 
   => dssp3 class_mapping = {0:"H",1:"E",2:"L"} 
   """
-  return torch.max(logits, dim=1 )[1].detach().cpu().numpy().squeeze()
+  preds = torch.max(logits, dim=1 )[1].detach().cpu().numpy().squeeze()
+  return preds
 
 def label_to_id(labels: str):
   """
   'HHEELLL' -> [0, 0, 1, 1, 2, 2, 2]
   """
   class_mapping = {"H":0, "E":1, "L":2, "C":2} 
-  converted = [class_mapping[c] for c in labels]
-  return converted
+  converted = [[class_mapping[c] for c in label] for label in labels]
+  return torch.tensor(converted)
 
 def custom_collate(data):
   # https://python.plainenglish.io/understanding-collate-fn-in-pytorch-f9d1742647d3
@@ -90,13 +96,48 @@ def custom_collate(data):
 
   return inputs, labels
 
-def main_training_loop(model, train_data, device):
-    bs = 4
-    lr = 0.01
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    epochs = 1
+def pad_infinite(iterable, padding=None):
+   return chain(iterable, repeat(padding))
+
+def pad(iterable, size, padding=None):
+   return islice(pad_infinite(iterable, padding), size)
+
+def process_label(labels: list):
+  # a list of labels ['HEECCC', 'HHHEEEECCC']
+  max_len = len(max(labels, key=len))
+  class_mapping = {"H":[1, 0, 0], "E":[0, 1, 0], "L":[0, 0, 1], "C":[0, 0, 1]}
+  processed = [[class_mapping[c] for c in label] for label in labels]
+  # add padding
+  padded = [list(pad(subl, max_len, [0, 0, 0])) for subl in processed]
+  return torch.tensor(np.array(padded), dtype=torch.float)
+
+
+def main_training_loop(model, train_data, val_data, device):
+    bs = 80
+    lr = 0.002
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    epochs = 15
+
+    config = {"learning_rate": lr,
+              "epochs": epochs,
+              "batch_size": bs,
+              "optimizer": optimizer}
+    wandb.init(project="emb_fex", entity="kyttang", config=config)
+    
+
     for i in range(epochs):
-        train(model, train_data, optimizer)
+      # train
+      t_loss = train(model, train_data, optimizer)
+      # wandb.log({"train_loss":t_loss})
+      # print("train_loss: ", t_loss)
+
+      # validate results
+      q3_accuracy, v_loss = validate(model, val_data)
+      # print("accuracy: ", q3_accuracy)
+      wandb.log({"accuracy (Q3)":q3_accuracy})
+
+
+
 
 
 def train(model: torch.nn.Module,
@@ -112,9 +153,10 @@ def train(model: torch.nn.Module,
 
     model.train()
     optimizer.zero_grad()
+    loss_fn = nn.CrossEntropyLoss()
 
     all_targets = list()
-    total_loss = 0
+    losses = []
 
     for i, batch in enumerate(train_data):
         batch_loss = 0
@@ -122,23 +164,65 @@ def train(model: torch.nn.Module,
         optimizer.zero_grad()
         out = model(emb)
 
+        labels = process_label(label)
+
+        loss = loss_fn(out, labels)
+        loss.backward()
+        
+        # print(loss.item())
+        losses.append(loss.item())
+        wandb.log({"train_loss":loss.item()})
+
+        optimizer.step()
+
         # print("out: ", out) # out.shape: torch.Size(bs, max_batch_seq_len, 3)
         # calculate loss for each sequence in the batch
-        for batch_idx, e in enumerate(out): ## Geht das nicht auch schneller?
-          # 'e' are 3 class logits of a sequence
+        # for batch_idx, out_logits in enumerate(out): ## Geht das nicht auch schneller?
+        #   # 'e' are 3 class logits of a sequence
+        #   optimizer.zero_grad()
+
+        #   seqlen = len(label[batch_idx])
+        #   # preds = logits_to_preds(out_logits[:seqlen]) # already in form: [0, 1, 2, 3]
+        #   true_label = label_to_id(label[batch_idx]) # convert label to machine readable.
+
+        #   # calculate loss for a sequece
+        #   # assert out_logits.size()[0] == true_label.size()
+        #   loss = loss_fn(out_logits[:seqlen], true_label)
+        #   loss.backward()
+        #   print("loss: ", loss.item())
+        #   optimizer.step()
+    return sum(losses)/len(losses)
+
+def validate(model: torch.nn.Module,
+          train_data: DataLoader):
+    model.eval()
+    for i, batch in enumerate(train_data):
+      emb, label = batch
+      out = model(emb)
+      acc_scores = []
+
+      for batch_idx, out_logits in enumerate(out): ## Geht das nicht auch schneller?
 
           seqlen = len(label[batch_idx])
-          preds = logits_to_preds(e[:seqlen]) # already in form: [0, 1, 2, 3]
+          preds = logits_to_preds(out_logits[:seqlen]) # already in form: [0, 1, 2, 3]
           true_label = label_to_id(label[batch_idx]) # convert label to machine readable.
 
-          # calculate loss for a sequece
-          ## TODO: find loss function for loss([0, 1, 1, 3], [1, 1, 1, 3])
+          acc = q3_acc(true_label, preds)
+          acc_scores.append(acc)
+      
+      
+      return sum(acc_scores)/len(acc_scores), -1
 
-def validate():
-    pass
+def q3_acc(y_true, y_pred):
+  return accuracy_score(y_true, y_pred)
 
+def sov(y_true, y_pred):
+  pass
 
 if __name__ == "__main__":
+    ## Collect garbage
+    gc.collect()
+
     ## Determine device
     print("Determine device")
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
@@ -151,23 +235,24 @@ if __name__ == "__main__":
     train_labels_path = "data/train.jsonl"
     val_labels_path = "data/val.jsonl"
 
-    # train_loader = get_dataloader(embed_path=train_embeds_path, 
-    #   labels_path=train_labels_path, batch_size=40, device=device, seed=42)
+    train_loader = get_dataloader(embed_path=train_embeds_path, 
+      labels_path=train_labels_path, batch_size=40, device=device, seed=42)
 
-    # val_loader = get_dataloader(embed_path=val_embeds_path, 
-    #  labels_path=val_labels_path, batch_size=40, device=device, seed=42)
+    val_loader = get_dataloader(embed_path=val_embeds_path, 
+     labels_path=val_labels_path, batch_size=40, device=device, seed=42)
 
     ### Test loader
-    test_embeds_path = "/content/drive/MyDrive/BachelorThesis/data/new_pisces.jsonl_embeddings.h5"
-    test_labels_path = "data/new_pisces.jsonl"
-    test_loader = get_dataloader(embed_path=test_embeds_path, 
-      labels_path=test_labels_path, batch_size=4, device=device, seed=42)
+    # test_embeds_path = "/content/drive/MyDrive/BachelorThesis/data/new_pisces.jsonl_embeddings.h5"
+    # test_labels_path = "data/new_pisces.jsonl"
+    # test_loader = get_dataloader(embed_path=test_embeds_path, 
+    #   labels_path=test_labels_path, batch_size=4, device=device, seed=42)
+    # ###
 
     ## Load model
     print("load Model")
     cnn = ConvNet()
 
-    ## Load Dataset (train and validate)
+    ## Train and validate (train and validate)
     print("start Training")
-    main_training_loop(model=cnn, train_data=test_loader, device=device)
+    main_training_loop(model=cnn, train_data=train_loader, val_data=val_loader, device=device)
     ##
