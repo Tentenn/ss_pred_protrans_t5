15aff2038059ab27f6f01c3710df34da021ec73e
Tentenn <kok.tang@gmx.de>
2022-06-21 22:43:06 +0000

    add testing functionality, automatic saving of checkpoints, function to turn predictions to human readablesequence

--- train_script.py
+++ train_script.py
@@ -26,8 +26,14 @@ import wandb
 
 import utils
 from ConvNet import ConvNet
+from ConvNet2 import ConvNet2 # Old convnet
 
 gc.enable()
+
+"""
+This code trains the CNN for 3-State secondary structure prediction
+Using ProtTrans T5 per residue embeddings.
+"""
 
 class EmbedDataset(Dataset):
     """
@@ -85,12 +91,14 @@ def label_to_id(labels: str):
   return torch.tensor(converted)
 
 def custom_collate(data):
+  """
   # https://python.plainenglish.io/understanding-collate-fn-in-pytorch-f9d1742647d3
-
-  # data is a list of batch size containing 2-tuple containing embedding and label seq
+  # data is a list of batch size containing 2-tuple 
+  # containing embedding and label seq
+  """
 
   inputs = [torch.tensor(d[0]) for d in data] # converting embeds to tensor
-  inputs = pad_sequence(inputs, batch_first=True) # pad embeddings to longest batch
+  inputs = pad_sequence(inputs, batch_first=True) # pad to longest batch
   
   labels = [d[1] for d in data] # TODO: convert seq to (seqlen, 3) 
 
@@ -103,7 +111,11 @@ def pad(iterable, size, padding=None):
    return islice(pad_infinite(iterable, padding), size)
 
 def process_label(labels: list):
-  # a list of labels ['HEECCC', 'HHHEEEECCC']
+  """
+  turns a list of labels ['HEECCC', 'HHHEEEECCC'] to one hot encoded tensor
+  and add padding e.g torch.tensor([[1, 0, 0], [0, 0, 1], [0, 0, 0]])
+  """
+
   max_len = len(max(labels, key=len))
   class_mapping = {"H":[1, 0, 0], "E":[0, 1, 0], "L":[0, 0, 1], "C":[0, 0, 1]}
   processed = [[class_mapping[c] for c in label] for label in labels]
@@ -112,32 +124,46 @@ def process_label(labels: list):
   return torch.tensor(np.array(padded), dtype=torch.float)
 
 
-def main_training_loop(model, train_data, val_data, device):
+def main_training_loop(model: torch.nn.Module, 
+                       train_data: DataLoader, 
+                       val_data: DataLoader, 
+                       device):
     bs = 80
-    lr = 0.002
+    lr = 0.003
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-    epochs = 15
+    epochs = 8
 
+    # wandb logging
     config = {"learning_rate": lr,
               "epochs": epochs,
               "batch_size": bs,
               "optimizer": optimizer}
     wandb.init(project="emb_fex", entity="kyttang", config=config)
-    
+    # track best scores
+    best_accuracy = float('-inf')
+    best_loss = float('-inf')
 
-    for i in range(epochs):
-      # train
+    for epoch in range(epochs):
+      # train model
       t_loss = train(model, train_data, optimizer)
-      # wandb.log({"train_loss":t_loss})
-      # print("train_loss: ", t_loss)
 
       # validate results
       q3_accuracy, v_loss = validate(model, val_data)
-      # print("accuracy: ", q3_accuracy)
       wandb.log({"accuracy (Q3)":q3_accuracy})
+      
+      # save model if better
+      if q3_accuracy > best_accuracy:
+        
+        EPOCH = 5
+        PATH = f"{bs}_{lr}_{epochs}_{round(q3_accuracy, 1)}_{t_loss}_cnn.pt"
+        LOSS = 0.4
 
-
-
+        torch.save({
+                    'epoch': epoch,
+                    'model_state_dict': model.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'loss': t_loss,
+                    }, PATH)
 
 
 def train(model: torch.nn.Module,
@@ -159,7 +185,7 @@ def train(model: torch.nn.Module,
     losses = []
 
     for i, batch in enumerate(train_data):
-        batch_loss = 0
+
         emb, label = batch
         optimizer.zero_grad()
         out = model(emb)
@@ -169,55 +195,119 @@ def train(model: torch.nn.Module,
         loss = loss_fn(out, labels)
         loss.backward()
         
-        # print(loss.item())
         losses.append(loss.item())
-        wandb.log({"train_loss":loss.item()})
+        wandb.log({"train_loss":loss.item()}) # logs loss for each batch
 
         optimizer.step()
-
-        # print("out: ", out) # out.shape: torch.Size(bs, max_batch_seq_len, 3)
-        # calculate loss for each sequence in the batch
-        # for batch_idx, out_logits in enumerate(out): ## Geht das nicht auch schneller?
-        #   # 'e' are 3 class logits of a sequence
-        #   optimizer.zero_grad()
-
-        #   seqlen = len(label[batch_idx])
-        #   # preds = logits_to_preds(out_logits[:seqlen]) # already in form: [0, 1, 2, 3]
-        #   true_label = label_to_id(label[batch_idx]) # convert label to machine readable.
-
-        #   # calculate loss for a sequece
-        #   # assert out_logits.size()[0] == true_label.size()
-        #   loss = loss_fn(out_logits[:seqlen], true_label)
-        #   loss.backward()
-        #   print("loss: ", loss.item())
-        #   optimizer.step()
     return sum(losses)/len(losses)
 
 def validate(model: torch.nn.Module,
-          train_data: DataLoader):
+          val_data: DataLoader):
     model.eval()
-    for i, batch in enumerate(train_data):
+    loss_fn = nn.CrossEntropyLoss()
+    for i, batch in enumerate(val_data):
       emb, label = batch
       out = model(emb)
       acc_scores = []
 
-      for batch_idx, out_logits in enumerate(out): ## Geht das nicht auch schneller?
+      labels = process_label(label)
+      loss = loss_fn(out, labels)
+      wandb.log({"val_loss":loss.item()})
 
-          seqlen = len(label[batch_idx])
-          preds = logits_to_preds(out_logits[:seqlen]) # already in form: [0, 1, 2, 3]
-          true_label = label_to_id(label[batch_idx]) # convert label to machine readable.
+      for batch_idx, out_logits in enumerate(out):
+        # Calculate scores for each sequence individually
+        # And average over them
 
-          acc = q3_acc(true_label, preds)
-          acc_scores.append(acc)
+        seqlen = len(label[batch_idx])
+        preds = logits_to_preds(out_logits[:seqlen]) # already in form: [0, 1, 2, 3]
+        true_label = label_to_id(label[batch_idx]) # convert label to machine readable.
+
+        acc = q3_acc(true_label, preds)
+        acc_scores.append(acc)
       
+    return sum(acc_scores)/len(acc_scores), np.std(acc_scores)
+
+def test(model: torch.nn.Module,
+          test_data: DataLoader,
+         verbose=False):
+    """
+    verbose argument: whether or not to show actual predictions
+    """
+    model.eval()
+    for i, batch in enumerate(test_data):
+      emb, label = batch
+      out = model(emb)
+      acc_scores = []
+      for batch_idx, out_logits in enumerate(out):
+        # Calculate scores for each sequence individually
+        # And average over them
+
+        seqlen = len(label[batch_idx])
+        preds = logits_to_preds(out_logits[:seqlen]) # already in form: [0, 1, 2, 3]
+        true_label = label_to_id(label[batch_idx]) # convert label to machine readable.
+
+        acc = q3_acc(true_label, preds)
+        acc_scores.append(acc)
+
+        if verbose:
+          print(f"prediction:\t", preds_to_seq(preds))
+          print(f"true label:\t", label[batch_idx])
+          print()
       
-      return sum(acc_scores)/len(acc_scores), -1
+    return sum(acc_scores)/len(acc_scores), np.std(acc_scores)
+
+def preds_to_seq(preds):
+  class_dict = {0:"H",1:"E",2:"C"}
+  return "".join([class_dict[c.item()] for c in preds.reshape(-1)])
 
 def q3_acc(y_true, y_pred):
   return accuracy_score(y_true, y_pred)
 
 def sov(y_true, y_pred):
   pass
+
+def load_sec_struct_model(path: str, model_type: torch.nn.Module):
+  checkpoint_dir=path
+  state = torch.load(checkpoint_dir, map_location=torch.device('cpu'))
+  model = model_type
+  if type(model_type) == ConvNet2:
+    model.load_state_dict(state['state_dict'])
+  elif type(model_type) == ConvNet:
+    model.load_state_dict(state['model_state_dict'])
+  else:
+    raise Exception
+  model = model.eval()
+  model = model.to(device)
+  # print('Loaded sec. struct. model from epoch: {:.1f}'.format(state['epoch']))
+
+  return model
+
+def predict_test_set():
+  ## Test loader
+  model_my = load_sec_struct_model("/content/drive/MyDrive/BachelorThesis/models/80_0.003_8_0.9_432.8474462689686_cnn.pt", ConvNet())
+
+
+  # Predict on CASP12
+  test_embeds_path = "/content/drive/MyDrive/BachelorThesis/data/casp12.jsonl_embeddings.h5"
+  test_labels_path = "data/casp12.jsonl"
+  test_loader = get_dataloader(embed_path=test_embeds_path, 
+                                labels_path=test_labels_path, 
+                                batch_size=4, device=device, seed=42)
+
+  acc, std = test(model_my, test_loader, verbose=True)
+  print("CASP12", acc, std)
+
+
+
+  # Predict on new_pisces
+  test2_embeds_path = "/content/drive/MyDrive/BachelorThesis/data/new_pisces.jsonl_embeddings.h5"
+  test2_labels_path = "data/new_pisces.jsonl"
+  test2_loader = get_dataloader(embed_path=test2_embeds_path, 
+                                labels_path=test2_labels_path, 
+                                batch_size=4, device=device, seed=42)
+
+  acc, std = test(model_my, test2_loader, verbose=True)
+  print("NEW_PISCES", acc, std)
 
 if __name__ == "__main__":
     ## Collect garbage
@@ -255,4 +345,6 @@ if __name__ == "__main__":
     ## Train and validate (train and validate)
     print("start Training")
     main_training_loop(model=cnn, train_data=train_loader, val_data=val_loader, device=device)
-    ##
+    
+    ## Test on the available test_sets
+    # predict_test_set()
