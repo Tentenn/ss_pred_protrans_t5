bd22581d7af7ee2e297e5f1f7550609f5f6b579e
Tentenn <kok.tang@gmx.de>
2022-06-26 22:19:35 +0000

    add SequenceDataset class for ft training

--- Dataset.py
+++ Dataset.py
@@ -29,3 +29,33 @@ class EmbedDataset(Dataset):
 
     def __len__(self) -> int:
         return len(self.headers)
+
+class SequenceDataset(Dataset):
+    """
+    SequenceDataset:
+    This dataset returns the sequence, labels and mask
+    """
+
+    def __init__(self, jsonl_path: str, 
+                 device: torch.device):
+        self.device = device
+        self.data_dict = utils.load_all_data(jsonl_path)
+        self.headers = tuple(self.data_dict.keys())
+        # Header refers to uniprot id
+        assert len(self.headers) == len(self.data_dict.keys()), "dict len not the same"
+
+    def __getitem__(self, index: int) -> Tuple[torch.Tensor, int, str]:
+        # given an index, return ids, label, mask
+        header = self.headers[index]
+        sequence, label, mask = self.data_dict[header]
+        tokenizer = T5Tokenizer.from_pretrained("Rostlab/prot_t5_xl_half_uniref50-enc")
+
+        # Preprocess sequence to ProtTrans format
+        sequence = " ".join(sequence)
+        prepro = re.sub(r"[UZOB]", "X", sequence)
+        ids = tokenizer.encode(prepro)
+
+        return ids, label, mask
+
+    def __len__(self) -> int:
+        return len(self.headers)
