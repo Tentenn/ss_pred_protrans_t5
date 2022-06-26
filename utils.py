a207cc7e6b543d7ac01d819d3df8da761dd54c6f
Tentenn <kok.tang@gmx.de>
2022-06-26 16:37:25 +0000

    add load_embeddings2

--- utils.py
+++ utils.py
@@ -10,18 +10,20 @@ def load_data(jsonl_path):
   example: 5t87-E CCCCCCHHHHHHCCEEEECCEEEEEEECCCCCCEEEEEEEECCEEEEEEECHHHCCHHHHHHHHCCCCCCCCCCCC
   """
   with open(jsonl_path) as t:
-        data_dict = dict()
-        ## Converts the list of dicts (jsonl file) to a single dict with id -> sequence
-        for d in [json.loads(line) for line in t]:
-            data_dict[d["id"]] = d["label"], d["resolved"]
+    data_dict = dict()
+    ## Converts the list of dicts (jsonl file) to a single dict with id -> sequence
+    for d in [json.loads(line) for line in t]:
+      data_dict[d["id"]] = d["label"], d["resolved"]
   return data_dict
 
 def load_embeddings(embeddings_path):
-    with h5py.File(embeddings_path, 'r') as f:
-        embeddings_dict = {seq_identifier: torch.tensor(np.array(f[seq_identifier])) for seq_identifier in f.keys()}
-    return embeddings_dict
+  with h5py.File(embeddings_path, 'r') as f:
+    embeddings_dict = {seq_identifier: torch.tensor(np.array(f[seq_identifier])) for seq_identifier in f.keys()}
+  return embeddings_dict
 
-if __name__ == "__main__":
-    lb_dict = load_labels("data/casp12.jsonl")
-    for k,v in lb_dict.items():
-        print(k,v)
+def load_embeddings2(embeddings_path):
+  embeddings_dict = dict()
+  with h5py.File(embeddings_path, 'r') as f:
+    for k in f.keys():
+      embeddings_dict[k] = f[k]
+  return embeddings_dict
