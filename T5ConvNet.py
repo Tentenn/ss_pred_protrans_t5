425063608ed5b1c0cc1a771fea3de0db92ae8b66
Tentenn <kok.tang@gmx.de>
2022-06-26 22:18:20 +0000

    trim last index of input ids

--- T5ConvNet.py
+++ T5ConvNet.py
@@ -10,8 +10,7 @@ class T5CNN(torch.nn.Module):
     def __init__(self):
         super(T5CNN, self).__init__()
 
-        self.t5 = T5EncoderModel.from_Pretrained("Rostlab/prot_t5_xl_half_uniref50-enc")
-        self.tokenizer = T5Tokenizer.from_pretrained("Rostlab/prot_t5_xl_half_uniref50-enc")
+        self.t5 = T5EncoderModel.from_pretrained("Rostlab/prot_t5_xl_half_uniref50-enc")
 
         self.elmo_feature_extractor = torch.nn.Sequential(
             torch.nn.Conv2d(1024, 32, kernel_size=(7, 1), padding=(3, 0)),  # 7x32
@@ -23,21 +22,22 @@ class T5CNN(torch.nn.Module):
             torch.nn.Conv2d(n_final_in, 3, kernel_size=(7, 1), padding=(3, 0))  # 7
         )
 
+    def forward(self, input_ids, attention_mask):
+        # trim ids (last item of each seq)
+        input_ids = input_ids[:,:-1]
+        assert input_ids.shape == attention_mask.shape, f"input_ids: {input_ids.shape}, mask: {attention_mask.shape}"
+        
+        # create embeddings
+        emb = self.t5(input_ids, attention_mask=attention_mask).last_hidden_state
 
-    def encode(self):
-        ## Encode sequence like in signalp6 or in code
-        return ids,
+        # remove special tokens
 
-    def forward(self, x):
-        # IN: X = (B x L x F);
-        # OUT: (B x F x L, 1)
-        x = x.permute(0, 2, 1).unsqueeze(dim=-1) ## ???
+        # dropout?
 
-        """
-        Create embeddings 
-        """
+        # padding
 
-        """
-        backward function
-        """
-        d3_Yhat = self.dssp3_classifier(x).squeeze(dim=-1).permute(0, 2, 1)  # OUT: (B x L x 3)
+        # old architecture
+        emb = emb.permute(0, 2, 1).unsqueeze(dim=-1)
+        emb = self.elmo_feature_extractor(emb)  # OUT: (B x 32 x L x 1)
+        d3_Yhat = self.dssp3_classifier(emb).squeeze(dim=-1).permute(0, 2, 1)  # OUT: (B x L x 3)
+        return d3_Yhat
