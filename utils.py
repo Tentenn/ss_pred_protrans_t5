dcdc7c2dc9dec5709beea56855fc8dba244fcdba
Tentenn <kok.tang@gmx.de>
2022-09-28 10:17:57 +0000

    fix device bug in utils

--- utils.py
+++ utils.py
@@ -41,7 +41,7 @@ def add_noise_embedding(embedding, device, density=0.2, mode="dropout", std=0.1,
   elif mode=="noise":
     return embedding + (std**variance)*torch.randn(embedding.shape, device=device)
   elif mode=="residue":
-    mask = torch.randn(embedding.shape[:2]).ge(density).unsqueeze(-1)
+    mask = torch.randn(embedding.shape[:2], device=device).ge(density).unsqueeze(-1)
     return mask*embedding
   else:
     assert False, "Not implemented dropout mode '{mode}'. available dropout, noise, residue"
